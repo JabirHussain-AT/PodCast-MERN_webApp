836904ac2370db3c559e86a01af3360bcc57e4c7
Jabirrr <jabirmhd6786@gmail.com>
2024-08-11 18:20:21 +0530

    feat : -bug fixed

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,5 @@
-const BACKEND_URL = 'http://localhost:3003'
+// const BACKEND_URL = 'http://localhost:3003'
+const BACKEND_URL = 'https://pod-cast-mern-web-app.vercel.app'
 
 export {
     BACKEND_URL
